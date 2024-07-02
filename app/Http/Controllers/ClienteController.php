06461506359576fe7625a5ba45fe9feed863245b
eugabriellyr <gabyynog2211@gmail.com>
2024-07-02 20:49:11 -0300

    ClienteController: Api de Login

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -28,6 +28,53 @@ class ClienteController extends Controller
         return view('site.dashboard.cliente.cliente', compact('cliente', 'usuario'));
     }
 
+    //API
+    public function show($id)
+    {
+        $cliente = Cliente::find($id);
+
+        if (!$cliente) {
+            return response()->json(['message' => 'Cliente não encontrado'], 404);
+        }
+
+        return response()->json($cliente, 200);
+
+    }
+    public function login(Request $request)
+{
+    $credentials = $request->validate([
+        'emailUsuario' => 'required|email',
+        'senhaUsuario' => 'required',
+    ]);
+
+    $usuario = Usuario::where('emailUsuario', $credentials['emailUsuario'])
+                      ->where('senhaUsuario', $credentials['senhaUsuario'])
+                      ->first();
+
+    if ($usuario && $usuario->tipoUsuario_type == 'cliente') {
+        $cliente = $usuario->tipo_usuario()->first();
+        if ($cliente) {
+            return response()->json([
+                'message' => 'Login bem sucedido',
+                'user' => [
+                    'id' => $usuario->idUsuario,
+                    'nome' => $usuario->nomeUsuario,
+                    'email' => $usuario->emailUsuario,
+                    'tipo' => $usuario->tipoUsuario_type,
+                    'dados_cliente' => [
+                        'idCliente' => $cliente->idCliente,
+                        'nome' => $cliente->nomecliente,
+                    ],
+                ],
+                'access_token' => $usuario->createToken('auth_token')->plainTextToken,
+                'token_type' => 'Bearer',
+            ]);
+        }
+    }
+    return response()->json(['data' => ['message' => 'Credenciais inválidas ou usuário não é um cliente']], 401);
+}
+
+
     public function agendar(){
 
         $idCliente = session('id');
